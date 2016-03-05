cd8f07423a2da1bc540c02a0befe069c50969e7a
Mohamed Amine SELMI <amine.selmi@gmail.com>
2016-03-05 23:08:12 +0100

    adding skills section

---
+++ public/scripts/mainpage.js
@@ -0,0 +1,15 @@
+
+
+    $(document).ready(function(){
+    	 'use strict';
+    /*  ----------------------------------------
+         Tooltip Starter for Social Media Icons
+        ----------------------------------------  */
+
+    $('.intro-content .social-media [data-toggle="tooltip"]').tooltip({
+        placement: 'bottom'
+    });
+
+    $('.contact-details .social-media [data-toggle="tooltip"]').tooltip();
+
+});
