e0d3d8d411027e61008bcd1283712614e5620296
MedXAmine <amine.selmi@gmail.com>
2016-03-19 13:06:26 +0100

    Modifiying porject folder name

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.get('/', function(request, response) {
 
 var pg = require('pg');
 
+
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
