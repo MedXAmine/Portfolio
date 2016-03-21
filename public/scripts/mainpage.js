24925fd602ba6716cec9103cbd10c1fbcf9def72
MedXAmine <amine.selmi@gmail.com>
2016-03-22 00:54:05 +0100

    Add carousel behavior

--- public/scripts/mainpage.js
+++ public/scripts/mainpage.js
@@ -1,7 +1,10 @@
 
 
     $(document).ready(function(){
-    	 'use strict';
+
+    'use strict';
+
+
     /*  ----------------------------------------
          Tooltip Starter for Social Media Icons
         ----------------------------------------  */
@@ -13,5 +16,36 @@
     $('.contact-details .social-media [data-toggle="tooltip"]').tooltip();
 
     $('#nav').localScroll({duration:800});
+
+
+
+ $("#blogSlider").owlCarousel({
+        items: 3,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 2],
+        itemsTablet: [768, 1],
+        itemsMobile: [479, 1],
+        slideSpeed: 500,
+        pagination: !1,
+        navigation: !1,
+        rewindSpeed: 700
+    });
+
+    var owl = $('#blogSlider');
+    owl.owlCarousel();
+
+    // Go to the next item
+    $('#next').click(function() {
+    owl.trigger('next.owl.carousel');
+     })
+
+
+    // Go to the previous item
+    $('#prev').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+})
+
 
 });
