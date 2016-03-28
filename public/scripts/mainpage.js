fe8cb3d44b652692db537097f11d5eb5af51caeb
MedXAmine <amine.selmi@gmail.com>
2016-03-28 23:55:52 +0200

    Changing references to Bower_components and configuration of Gulp task runner

--- public/scripts/mainpage.js
+++ public/scripts/mainpage.js
@@ -1,6 +1,4 @@
-
-
-    $(document).ready(function(){
+$(document).ready(function () {
 
     'use strict';
 
@@ -15,57 +13,58 @@
 
     $('.contact-details .social-media [data-toggle="tooltip"]').tooltip();
 
-    $('#nav').localScroll({duration:800});
+    $('#nav').localScroll({
+        duration: 800
+    });
 
 
 
 
     $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    slideSpeed: 500,
-    responsiveClass:true,
-    slideSpeed: 500,
-    pagination: !1,
-    navigation: !1,
-    rewindSpeed: 700,
-    nav:false,
-    responsive:{
-        0:{
-            items:1,
-        
-        },
-        600:{
-            items:1,
-          
-        },
-        979:{
-            items:2,
-         
-        },
-        1199:{
-            items:3,
-         
-            
+        loop: true,
+        margin: 10,
+        slideSpeed: 500,
+        responsiveClass: true,
+        pagination: !1,
+        navigation: !1,
+        rewindSpeed: 700,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1,
+
+            },
+            600: {
+                items: 1,
+
+            },
+            979: {
+                items: 2,
+
+            },
+            1199: {
+                items: 3,
+
+
+            }
         }
-    }
-})
+    })
 
     var owl = $('#blogSlider');
     owl.owlCarousel();
 
     // Go to the next item
-    $('#next').click(function() {
-    owl.trigger('next.owl.carousel');
-     })
+    $('#next').click(function () {
+        owl.trigger('next.owl.carousel');
+    });
 
 
     // Go to the previous item
-    $('#prev').click(function() {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
-    owl.trigger('prev.owl.carousel', [300]);
-})
+    $('#prev').click(function () {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    });
 
 
 });
---
+++ src/scripts/mainpage.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+
+    'use strict';
+
+
+    /*  ----------------------------------------
+         Tooltip Starter for Social Media Icons
+        ----------------------------------------  */
+
+    $('.intro-content .social-media [data-toggle="tooltip"]').tooltip({
+        placement: 'bottom'
+    });
+
+    $('.contact-details .social-media [data-toggle="tooltip"]').tooltip();
+
+    $('#nav').localScroll({
+        duration: 800
+    });
+
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        slideSpeed: 500,
+        responsiveClass: true,
+        pagination: !1,
+        navigation: !1,
+        rewindSpeed: 700,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1,
+
+            },
+            600: {
+                items: 1,
+
+            },
+            979: {
+                items: 2,
+
+            },
+            1199: {
+                items: 3,
+
+
+            }
+        }
+    })
+
+    var owl = $('#blogSlider');
+    owl.owlCarousel();
+
+    // Go to the next item
+    $('#next').click(function () {
+        owl.trigger('next.owl.carousel');
+    });
+
+
+    // Go to the previous item
+    $('#prev').click(function () {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    });
+
+
+});
